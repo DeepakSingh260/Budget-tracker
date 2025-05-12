ab924638393d75011283fda5fcdefb71aa4b2d98
DeepakSingh260 <alexmha260@gmail.com>
2025-05-13 03:26:20 +0530

    api

--- backend/budget_tracker/budget_tracker/urls.py
+++ backend/budget_tracker/budget_tracker/urls.py
@@ -17,7 +17,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework.routers import DefaultRouter
+from .views import CategoryViewSet, TransactionViewSet, BudgetViewSet
+
+router = DefaultRouter()
+router.register(r'categories', CategoryViewSet, basename='category')
+router.register(r'transactions', TransactionViewSet, basename='transaction')
+router.register(r'budgets', BudgetViewSet, basename='budget')
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
+    path('', include(router.urls)),
 ]
