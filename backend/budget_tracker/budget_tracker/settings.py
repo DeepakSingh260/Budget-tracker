c151af97fb3a00a78bf8e338e7ae1795718460fd
DeepakSingh260 <alexmha260@gmail.com>
2025-05-17 03:39:39 +0530

    create frontend with respect to the api's

--- backend/budget_tracker/budget_tracker/settings.py
+++ backend/budget_tracker/budget_tracker/settings.py
@@ -95,7 +95,7 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    
+    "https://budget-tracker-1-x1he.onrender.com",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
